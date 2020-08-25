dfeda356e57c8bc6a5e1e04dfe24f84ab07073d5
ghugan <619ghugan@gmail.com>
2020-08-25 16:46:33 +0800

    install chart js library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { BarChartComponent } from './widgets/bar-chart/bar-chart.component';
+import { ChartsModule } from 'ng2-charts';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
