86fba0998d325f5d1742e7a3294dc24e1fb091da
ghugan <619ghugan@gmail.com>
2020-08-25 16:49:23 +0800

    remoive d3 chart

--- src/app/widgets/bar-chart/bar-chart.component.ts
+++ src/app/widgets/bar-chart/bar-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-import * as d3 from 'd3';
+
 import { DataService } from 'src/app/services/data/data.service';
 
 @Component({
@@ -24,25 +24,15 @@ export class BarChartComponent implements OnInit {
   constructor(private oip: DataService) { }
 
   ngOnInit() {
-    this.createSvg();
+
     this.getdata();
 
   }
 
 
-
-  private createSvg(): void {
-    this.svg = d3.select("figure#bar")
-      .append("svg")
-      .attr("width", this.width + (this.margin * 2))
-      .attr("height", this.height + (this.margin * 2))
-      .append("g")
-      .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
-  }
-
   getdata() {
     this.oip.getCountryResult().then(res => {
-      this.drawBars(res as any);
+
       console.log(res);
     })
       .catch(error => {
@@ -50,39 +40,5 @@ export class BarChartComponent implements OnInit {
       })
   }
 
-  private drawBars(data: any[]): void {
-    // Create the X-axis band scale
-    const x = d3.scaleBand()
-      .range([0, this.width])
-      .domain(data.map(d => d.Country))
-      .padding(0.2);
 
-    // Draw the X-axis on the DOM
-    this.svg.append("g")
-      .attr("transform", "translate(0," + this.height + ")")
-      .call(d3.axisBottom(x))
-      .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
-
-    // Create the Y-axis band scale
-    const y = d3.scaleLinear()
-      .domain([0, 200000])
-      .range([this.height, 0]);
-
-    // Draw the Y-axis on the DOM
-    this.svg.append("g")
-      .call(d3.axisLeft(y));
-
-    // Create and fill the bars
-    this.svg.selectAll("bars")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("x", d => x(d.Country))
-      .attr("y", d => y(d.Deaths))
-      .attr("width", x.bandwidth())
-      .attr("height", (d) => this.height - y(d.Deaths))
-      .attr("fill", "#d04a35");
-  }
 }
