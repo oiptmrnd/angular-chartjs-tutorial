8917a725f205552a48fe32a1ed8caa4bcb764d6d
ghugan <619ghugan@gmail.com>
2020-08-26 13:38:47 +0800

    change title of angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-d3-tutorial';
+  title = 'angular-chartJs-tutorial';
 }
